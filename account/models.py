553f350480e1f2ef6cfe0ca26738af12399475d0
Ryan Skalla <rskalla95@gmail.com>
2019-04-09 11:58:26 -0600

    MIgrations

--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.http import HttpResponseRedirect
 
 # Create your models here.
+
+class User (AbstractUser):
+
+    birthDate = models.DateTimeField(null=True)
+    favcolor = models.TextField(null=True)
+
+
+
+        # return the Sale object
