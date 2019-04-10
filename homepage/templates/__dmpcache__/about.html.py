74ea830a4c60e0a855a21c62635391f560194f79
Steven Rummler <steven.rummler@gmail.com>
2019-04-09 21:21:36 -0600

    fixed urls in allowed hosts

--- homepage/templates/__dmpcache__/about.html.py
+++ homepage/templates/__dmpcache__/about.html.py
@@ -5,9 +5,9 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554830067.9463837
+_modified_time = 1554864014.8126674
 _enable_loop = True
-_template_filename = '/mnt/c/Users/rskal/IS413/Intex/homepage/templates/about.html'
+_template_filename = 'C:/Users/steve/Desktop/intex2/homepage/templates/about.html'
 _template_uri = 'about.html'
 _source_encoding = 'utf-8'
 import django_mako_plus
@@ -33,12 +33,12 @@ def render_body(context,**pageargs):
         def content():
             return render_content(context._locals(__M_locals))
         __M_writer = context.writer()
-        __M_writer('\n\n')
+        __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
             context['self'].content(**pageargs)
         
 
-        __M_writer('\n')
+        __M_writer('\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -50,7 +50,7 @@ def render_content(context,**pageargs):
         def content():
             return render_content(context)
         __M_writer = context.writer()
-        __M_writer('\n    <div class="jumbotron" style="max-width: 50%; margin-left: 25%; margin-right: 25%">\n        <h1> About Prescription Tracker</h1>\n        <br />\n        <p>The opioid crisis is devastating communities across America. The latest statistics indicate that the total yearly overdose deaths due to opioids reached a new high of 70,237 in 2017. In that same year, President Trump declared the opioid crisis a public health emergency. </p>\n            <br />\n        <p>While different measures have been taken to combat the crisis,no web-accessible database is available to allow government officials, healthcare providers, and prescribers to view and analyze the volume of drugs being prescribed... Until Now</p>\n            <br />\n        <p>OpiateTracker.co was built to help combat the opioid epidemic across america by providing government officials, healthcare providers, and prescribers to view and analyze the volume of drugs being prescribed.</p>\n    </div>\n\n\n')
+        __M_writer('\r\n    <div class="jumbotron" style="max-width: 50%; margin-left: 25%; margin-right: 25%">\r\n        <h1> About Prescription Tracker</h1>\r\n        <br />\r\n        <p>The opioid crisis is devastating communities across America. The latest statistics indicate that the total yearly overdose deaths due to opioids reached a new high of 70,237 in 2017. In that same year, President Trump declared the opioid crisis a public health emergency. </p>\r\n            <br />\r\n        <p>While different measures have been taken to combat the crisis,no web-accessible database is available to allow government officials, healthcare providers, and prescribers to view and analyze the volume of drugs being prescribed... Until Now</p>\r\n            <br />\r\n        <p>OpioidTracker.co was built to help combat the opioid epidemic across america by providing government officials, healthcare providers, and prescribers to view and analyze the volume of drugs being prescribed.</p>\r\n    </div>\r\n\r\n\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -58,6 +58,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "/mnt/c/Users/rskal/IS413/Intex/homepage/templates/about.html", "uri": "about.html", "source_encoding": "utf-8", "line_map": {"29": 0, "36": 1, "41": 15, "47": 3, "53": 3, "59": 53}}
+{"filename": "C:/Users/steve/Desktop/intex2/homepage/templates/about.html", "uri": "about.html", "source_encoding": "utf-8", "line_map": {"29": 0, "36": 1, "41": 15, "47": 3, "53": 3, "59": 53}}
 __M_END_METADATA
 """
