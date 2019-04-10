7a2d1139dcbe4c298d410066cc448d47d805182f
Ryan Skalla <rskalla95@gmail.com>
2019-04-10 17:50:37 -0600

    404

--- Intex/settings.py
+++ Intex/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(=+jcrqk9_dxsgqq%u!2oq1ycx0^78(v_zs%izzmcq-e&h+*d_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 AUTH_USER_MODEL = 'account.customUser'
 
